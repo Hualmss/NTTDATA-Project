e061e364e681c6fe60329645dc027c1e6766ac43
luis <laliagam@unsa.edu.pe>
2023-02-16 12:35:05 -0500

    microservice eureka

--- movimient/src/main/java/com/nttdata/movimients/MovimientApplication.java
+++ movimient/src/main/java/com/nttdata/movimients/MovimientApplication.java
@@ -2,8 +2,12 @@ package com.nttdata.movimients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovimientApplication {
 
 	public static void main(String[] args) {
